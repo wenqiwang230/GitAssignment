f64e61672a9c3c861215dc62838bc617ec485746
wenqiwang230 <wenqiwang1993@gmail.com>
2016-09-21 21:49:21 -0400

    Final version(mofied answers.txt and rolegame.java
    Correct an answer and add comments in rolegame.java

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -21,21 +21,15 @@ public class RoleGame {
         System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         //arrow
         System.out.println("Arrow has " + arrow.hit() + " of damage.");
-        armor = 3;
-        System.out.println("Arrow was able to do " + arrow.hit(armor) + " of damage due to an armor with "+armor+ " points.");
-        armor = 10;
         System.out.println("Arrow was able to do " + arrow.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         //axe
         System.out.println("Axe has " + axe.hit() + " of damage.");
-        armor = 18;
+ 
         System.out.println("Axe was able to do " + axe.hit(armor) + " of damage due to an armor with "+armor+ " points.");
-        armor = 21;
-        System.out.println("Axe was able to do " + axe.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+
         //magicstaff
         System.out.println("MagicStaff has " + magicstaff.hit() + " of damage.");
-        armor = 20;
-        System.out.println("MagicStaff was able to do " + magicstaff.hit(armor) + " of damage due to an armor with "+armor+ " points.");
-        armor = 11;
+
         System.out.println("MagicStaff was able to do " + magicstaff.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         //TODO: Add the remainig weapons here
 
